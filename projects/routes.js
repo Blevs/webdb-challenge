09106e1398f71e6092481a10908d6e3952fea1bc
Henry Blevins <heblevi@gmail.com>
2019-05-03 12:04:07 -0400

    Add project get by id route

--- projects/routes.js
+++ projects/routes.js
@@ -15,10 +15,19 @@ router.post('/', (req, res) => {
     project.completed = project.completed || false;
     Projects.insert(project)
       .then(project => res.status(201).json(project))
-      .catch(() => res.status(500).json({message: "Error creating project"}));
+      .catch(() => res.status(500).json({message: "Error creating project."}));
   } else {
     res.status(400).json({message: "Project requires name."});
   }
+});
+
+router.get('/:id', (req, res) => {
+  const {id} = req.params;
+  Projects.get(id)
+    .then(project => project
+          ? res.status(200).json(project)
+          : res.status(404).json({message: "No project with id."}))
+    .catch(() => res.status(500).json({message: "Error fetching project"}));
 });
 
 module.exports = router;
